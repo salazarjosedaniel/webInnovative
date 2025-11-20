73a64b32b6c065ac15e17beffc283380f1a89090
Jose Salazar <jose.salazar@indx.com>
2025-11-19 20:28:57 -0600

    Update tasas.js

--- api/tasas.js
+++ api/tasas.js
@@ -1,12 +1,20 @@
-import { getBCV_USD, getBCV_EUR } from "../../lib/bcv";
-
 export default async function handler(req, res) {
-  const usd = await getBCV_USD();
-  const eur = await getBCV_EUR();
+  try {
+    const r = await fetch("https://api.dolarvzla.com/public/exchange-rate");
+    const json = await r.json();
 
-  res.status(200).json({
-    usd,
-    eur,
-    fecha: new Date().toISOString()
-  });
+    const usd = json.current.usd.toFixed(2);
+    const eur = json.current.eur.toFixed(2);
+    const fecha = json.current.date;
+
+    return res.status(200).json({
+      usd,
+      eur,
+      fecha
+    });
+
+  } catch (err) {
+    console.error("❌ ERROR /api/tasas:", err);
+    return res.status(500).json({ error: "API error" });
+  }
 }
